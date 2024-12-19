1b5927bb53a481d01c1629694d5198b08caf38a8
Typher7 <111155443+Typher7@users.noreply.github.com>
2024-12-18 20:10:02 -0600

    add_blocks method added

--- testflask.py
+++ testflask.py
@@ -5,10 +5,10 @@ from blockchain import Blockchain
 app = Flask(__name__)
 
 # Init blockchain
-blockchain = Blockchain(difficulty=4)
+blockchain = Blockchain(difficulty=6)
 
 # Get the full blockchain
-@app.route('/chain', methods=['Get'])
+@app.route('/chain', methods=['GET'])
 def get_chain():
     chain_data = []
     for block in blockchain.chain:
@@ -20,7 +20,7 @@ def get_chain():
             "nonce": block.nonce,
             "timestamp": block.timestamp
         })
-    return jsonify({"length": len(chain_data), "chain": chain_data}), 200
+    return jsonify({"length": blockchain.length, "chain": chain_data}), 200
 
 # Add a new block
 @app.route('/add_block', methods=['POST'])
@@ -31,10 +31,21 @@ def add_block():
     blockchain.add_block(data)
     return jsonify({"message": "Block added successfully"}), 201
 
+# Add n blocks
+@app.route('/add_blocks/<int:n>', methods=['POST'])
+def add_blocks(n):
+    data = request.get_json().get("data")
+    if not data:
+        return jsonify({"error": "Data is required"}), 400
+    for i in range(n):
+        blockchain.add_block(data[i])
+
+    return jsonify({"message": "Blocks added successfully"}), 201
+
 # Validate the Chain
 @app.route('/validate', methods=["GET"])
 def validate_chain():
-    for i in range(1, len(blockchain.chain)):
+    for i in range(1, blockchain.length):
         current_block = blockchain.chain[i]
         prev_block = blockchain.chain[i-1]
 
@@ -45,6 +56,23 @@ def validate_chain():
             return jsonify({"valid": False, "error": "Broken chain"}), 400
     
     return jsonify({"valid": True}), 200
+
+# Query block by index
+@app.route('/block/<int:index>', methods=['GET'])
+def get_block(index):
+    if index < 0 or index >= blockchain.length:
+        return jsonify({"error": "Block doesn't exist"}), 404
+    
+    block = blockchain.chain[index]
+    block_data = {
+        "index": block.index,
+        "hash": block.hash,
+        "previous_hash": block.previous_hash,
+        "data": block.data,
+        "nonce": block.nonce,
+        "timestamp": block.timestamp
+    }
+    return jsonify(block_data), 200
 
 if __name__ == "__main__":
     app.run(port=5000)
